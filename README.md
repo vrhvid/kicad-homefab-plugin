c3ebbb82c018e05b110720ee857f4faacf78b0a7
vrhvid <vrhvid@gmail.com>
2024-07-10 18:23:23 +0200

    Prva verzija projekta

---
+++ README.md
@@ -0,0 +1,15 @@
+# Kicad HomeFab plugin
+Verzija: 0.1.0
+Datum: 10. 07. 2024
+
+Python program, ki se bo s časom (verjetno) razvil v vtičnik za program KiCad. 
+trenutno program združi Gerber datoteko z vezjem in Gerber datoteko za vrtanje v eno datoteko.
+
+## Zahteve
+- testirano na Python _3.12.0_
+
+## Uporaba 
+1. program mora biti v isti mapi kot so Gerber datoteke
+2. datoteka z vezjem mora biti poimenovana z _gerber.gbr_
+3. datoteka za vrtanje mora biti poimenovana z _dril.gbr_
+4. poženi program, ki bo ustvaril datoteko _homefab\_gerber.gbr_
---
+++ drill_hole_script.py
@@ -0,0 +1,30 @@
+import time
+
+with open("homefab_gerber.gbr", "w") as file1:
+    with open("gerber.gbr", "r") as file2:
+        while True:
+            line = file2.readline()
+            if line.strip() != "M02*":
+                file1.write(line)
+            else:
+                break
+
+    with open("dril.gbr", "r") as file3:
+        while True:
+            line = file3.readline()
+            
+            if line.strip() == "G04 APERTURE END LIST*":
+                break
+            else:
+                continue
+        
+        file1.write("%TA.AperFunction,Conductor*%\n%ADD10C,1.000000*%\n%TD*%\nD10*\n%LPC*%\n")
+
+        while True:
+            line = file3.readline()
+            if line.strip() == "":
+                break 
+            elif (line.strip())[0] == "X":
+                file1.write(line)
+    
+    file1.write("M02*")
